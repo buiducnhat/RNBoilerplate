67d93ae1a5ad9da33ce1bd10c3c47a39b246ac3c
Bui Duc Nhat <nhaths4701@gmail.com>
2021-08-09 14:19:50 +0700

    Add reudx persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import * as eva from '@eva-design/eva';
 import { customLightTheme, customDarkTheme } from './theme/customTheme';
 
 import { BottomNavigator } from './navigations/BottomNavigator';
-import { store } from './configs/redux/store';
+import { store, persistor } from './configs/redux/store';
 import { useAppSelector } from './hooks/reduxHooks';
 import { selectThemeType } from './features/ui/uiSlice';
 
@@ -26,10 +27,12 @@ export default (): React.ReactFragment => {
   return (
     <>
       <ReduxProvider store={store}>
-        <IconRegistry icons={EvaIconsPack} />
-        <ThemeProvider>
-          <BottomNavigator />
-        </ThemeProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <IconRegistry icons={EvaIconsPack} />
+          <ThemeProvider>
+            <BottomNavigator />
+          </ThemeProvider>
+        </PersistGate>
       </ReduxProvider>
     </>
   );
--- src/configs/redux/store.ts
+++ src/configs/redux/store.ts
@@ -1,12 +1,38 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import uiReducer from '@src/features/ui/uiSlice';
 
-export const store = configureStore({
-  reducer: {
-    ui: uiReducer
-  },
-  devTools: process.env.NODE_ENV !== 'production'
+const rootReducer = combineReducers({
+  ui: uiReducer
 });
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
+});
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
